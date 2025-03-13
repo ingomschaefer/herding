f715b0f88053ce66cfde060d9772dc9b15181f48
ingomschaefer <ingo.schaefer@rockstardevelopers.de>
2025-03-13 12:47:58 +0100

    Add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }]
+  }
+};
